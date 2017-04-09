218c5df619b7c3aaab32f2c72eb1b9b8abc93583
miramyllyla <mira.myllyla@aalto.fi>
2017-04-09 16:06:03 +0300

    s
    s

--- animation.js
+++ animation.js
@@ -103,6 +103,15 @@ $(document).ready( function() {
     window.addEventListener('keyup', function(e) {
         delete keysDown[e.keyCode];
     });
+    
+    var arrow_keys_handler = function(e) {
+    switch(e.keyCode){
+        case 37: case 39: case 38:  case 40: // Arrow keys
+        case 32: e.preventDefault(); break; // Space
+        default: break; // do not block other keys
+    }
+    };
+    window.addEventListener("keydown", arrow_keys_handler, false);
 
 
     
